1bbf3629ccd899e4ffb2d94ab0024e0b6a5292b9
julikaliti <yulikaliti@gmail.com>
2021-03-14 14:40:13 +0300

    feat: implement function for given brackets returns correct or not

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	if (str.length % 2 !== 0) return false;
+
+	const stack = [];
+	const map = new Map(bracketsConfig);
+
+	stack.push(str[0]);
+
+	for (let i = 1; i < str.length; i++) {
+		stack.push(str[i]);
+		if (stack[stack.length-1] == map.get(stack[stack.length-2])){
+			stack.pop();
+			stack.splice(stack.length-1, 1);
+		} 
+	}
+
+	if (stack.length === 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
